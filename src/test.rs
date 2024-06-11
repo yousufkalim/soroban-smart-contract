f761d2fe04f73ac3d605cf42448e06965e6099b8
haris036 <109869504+haris036@users.noreply.github.com>
2024-06-11 16:03:16 +0500

    Update test.rs

--- src/test.rs
+++ src/test.rs
@@ -1,242 +1,398 @@
 #![cfg(test)]
 
-use crate::Campaign;
 
-use super::testutils::{register_test_contract as register_contract, CrowdFund};
+use super::*;
+use soroban_sdk::{
+    testutils::{Address as _, Ledger, LedgerInfo},
+    token, Address, Env,
+};
+extern crate std;
 
-use soroban_sdk::token::Client;
+#[test]
+fn test_intialize() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    // Address::generate(&env);
 
-use super::CrowdFundClient;
-use soroban_sdk::{testutils::Address as _, token::Client as Token, Address, Env, String, Vec};
-mod token_arty {
-    soroban_sdk::contractimport!(file = "token/soroban_token_contract.wasm");
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
+
+    assert_eq!(initialized, String::from_str(&env, "Initialized"));
+    // let re_initialized: Result<String, Error> = core::prelude::v1::Ok(client.initialize(&admin.clone(), &admin.clone(), &admin.clone(),  &admin.clone()));
+
+    // assert_eq!(re_initialized, Err(Error::AlreadyInitialized));
 }
 
-fn create_contract() -> (
-    CrowdFundClient<'static>,
-    Env,
-    token_arty::Client<'static>,
-    Address,
-) {
+#[test]
+fn test_create_product() {
     let env = Env::default();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
     env.mock_all_auths();
+    let admin = Address::generate(&env);
+    // Address::generate(&env);
 
-    let id = register_contract(&env);
-    let crowdfund = CrowdFund::new(&env, id.clone());
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
 
-    // ARTY token creation
-    let token_admin = Address::random(&env);
-    let contract_arty = env.register_stellar_asset_contract(token_admin.clone());
-    let arty_token = token_arty::Client::new(&env, &contract_arty);
+    assert_eq!(initialized, String::from_str(&env, "Initialized"));
+
+    std::println!("{}", env.ledger().timestamp());
+    let title = String::from_str(&env, "Product 1");
+    let description = String::from_str(&env, "Description 1");
+    let category = String::from_str(&env, "Category 1");
+    let expiry = env.ledger().timestamp() + 10000;
+    let image = String::from_str(&env, "image.png");
+    let price = 1000;
+    let target = 10;
+
+    // initialize(env.clone(), admin.clone(), admin.clone(), admin.clone(), admin.clone());
+    let product = client.create_product(
+        // env.clone(),
+        &title.clone(),
+        &description.clone(),
+        &category.clone(),
+        &expiry,
+        &image.clone(),
+        &price,
+        &target,
+    );
+
+    assert_eq!(product.id, 1);
+    assert_eq!(product.title, title);
+    assert_eq!(product.description, description);
+    assert_eq!(product.category, category);
+    assert_eq!(product.expiry, expiry);
+    assert_eq!(product.image, image);
+    assert_eq!(product.price, price);
+    assert_eq!(product.remaining, target);
+}
+
+#[test]
+fn test_get_product() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    // Address::generate(&env);
+
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
+
+    assert_eq!(initialized, String::from_str(&env, "Initialized"));
+
+    let title = String::from_str(&env, "Product 1");
+    let description = String::from_str(&env, "Description 1");
+    let category = String::from_str(&env, "Category 1");
+    let expiry = env.ledger().timestamp() + 10000;
+    let image = String::from_str(&env, "image.png");
+    let price = 1000;
+    let target = 10;
+
+    // initialize(env.clone(), admin.clone(), admin.clone(), admin.clone(), admin.clone());
+    client.create_product(
+        // env.clone(),
+        &title.clone(),
+        &description.clone(),
+        &category.clone(),
+        &expiry,
+        &image.clone(),
+        &price,
+        &target,
+    );
+
+    let product = client.get_product(&1);
+    assert_eq!(product.id, 1);
+    assert_eq!(product.title, title);
+    assert_eq!(product.description, description);
+    assert_eq!(product.category, category);
+    assert_eq!(product.expiry, expiry);
+    assert_eq!(product.image, image);
+    assert_eq!(product.price, price);
+    assert_eq!(product.remaining, target);
+}
+
+#[test]
+fn test_get_products() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    // Address::generate(&env);
+
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
+
+    assert_eq!(initialized, String::from_str(&env, "Initialized"));
+
+    let title = String::from_str(&env, "Product 1");
+    let description = String::from_str(&env, "Description 1");
+    let category = String::from_str(&env, "Category 1");
+    let expiry = env.ledger().timestamp() + 10000;
+    let image = String::from_str(&env, "image.png");
+    let price = 1000;
+    let target = 10;
+
+    // initialize(env.clone(), admin.clone(), admin.clone(), admin.clone(), admin.clone());
+    client.create_product(
+        // env.clone(),
+        &title.clone(),
+        &description.clone(),
+        &category.clone(),
+        &expiry,
+        &image.clone(),
+        &price,
+        &target,
+    );
+
+    let title2 = String::from_str(&env, "Product 2");
+    let description2 = String::from_str(&env, "Description 2");
+    let category2 = String::from_str(&env, "Category 2");
+    let expiry2 = env.ledger().timestamp() + 20000;
+    let image2 = String::from_str(&env, "image2.png");
+    let price2 = 2000;
+    let target2 = 20;
+
+    client.create_product(
+        // env.clone(),
+        &title2.clone(),
+        &description2.clone(),
+        &category2.clone(),
+        &expiry2,
+        &image2.clone(),
+        &price2,
+        &target2,
+    );
+
+    let products = client.get_products();
+    assert_eq!(products.len(), 2);
+    assert_eq!(products.get(0).unwrap().title, title);
+    assert_eq!(products.get(1).unwrap().title, title2);
+}
+
+#[test]
+fn test_get_discount() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let token_id = Address::generate(&env);
+    // Address::generate(&env);
+    let customer = Address::generate(&env);
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
+    let token = token::StellarAssetClient::new(
+        &env,
+        &env.register_stellar_asset_contract(token_id.clone()),
+    );
 
     // Mint some ARTY tokens to work with
-    arty_token.mint(&token_admin, &50000);
-    // arty_token
+    token.mint(&customer.clone(), &10000000000);
 
-    // Accounts creation and initialization
-    let dev_acc = Address::random(&env);
-    let launchpad_acc = Address::random(&env);
+    assert_eq!(initialized, String::from_str(&env, "Initialized"));
 
-    let client = crowdfund.client();
+    let title = String::from_str(&env, "Product 1");
+    let description = String::from_str(&env, "Description 1");
+    let category = String::from_str(&env, "Category 1");
+    let expiry = env.ledger().timestamp() + 10000;
+    let image = String::from_str(&env, "image.png");
+    let price = 1000;
+    let target = 10;
+    let amount = 1;
+    // initialize(env.clone(), admin.clone(), admin.clone(), admin.clone(), admin.clone());
+    client.create_product(
+        // env.clone(),
+        &title.clone(),
+        &description.clone(),
+        &category.clone(),
+        &expiry,
+        &image.clone(),
+        &price,
+        &target,
+    );
 
-    // initialize the accounts, ARTY token and Admin Account
-    client.initialize(&dev_acc, &launchpad_acc, &contract_arty, &token_admin);
+    let (reserve_amount, launchpad_amount, dev_amount) =
+        client.get_discount(&1, &customer.clone(), &amount, &token.address.clone());
 
-    (client, env.clone(), arty_token, token_admin)
-}
+    let total_amount = amount * 10000000;
+    let expected_reserve_amount =
+        (total_amount * RESERVE_PER) / (RESERVE_PER + LAUNCHPAD_PER + DEV_PER);
+    let expected_launchpad_amount =
+        (total_amount * LAUNCHPAD_PER) / (RESERVE_PER + LAUNCHPAD_PER + DEV_PER);
+    let expected_dev_amount = (total_amount * DEV_PER) / (RESERVE_PER + LAUNCHPAD_PER + DEV_PER);
 
-struct Setup<'a> {
-    env: Env,
-    client: CrowdFundClient<'static>,
-    contract_token: Address,
-    token: Token<'a>,
-    arty_token: Client<'static>,
-    artist1: Address,
-    artist2: Address,
-    donor1: Address,
-    donor2: Address,
-    title: String,
-    description: String,
-    target: i128,
-    target1: i128,
-    deadline: u64,
-    image: String,
-    campaign1: Campaign,
-    campaign2: Campaign,
-    category: String,
-}
+    assert_eq!(reserve_amount, expected_reserve_amount);
+    assert_eq!(launchpad_amount, expected_launchpad_amount);
+    assert_eq!(dev_amount, expected_dev_amount);
 
-impl Setup<'_> {
-    fn new() -> Self {
-        let contract_client = create_contract();
-        let client = contract_client.0;
-        let env = contract_client.1;
-        let arty_token = contract_client.2;
-        let token_admin = contract_client.3;
-
-        // Create the token contract
-        let contract_token = env.register_stellar_asset_contract(token_admin);
-        let token = Token::new(&env, &contract_token);
-
-        // Artists Addresses
-        let artist1 = Address::random(&env);
-        let artist2 = Address::random(&env);
-
-        // Donors Addresses
-        let donor1 = Address::random(&env);
-        let donor2 = Address::random(&env);
-
-        // Mint some tokens to work with
-        token.mint(&donor1, &1000);
-        token.mint(&donor2, &500);
-
-        // Campaign Details
-        let title = String::from_slice(&env, "New Title");
-        let description = String::from_slice(&env, "New Desc");
-        let category = String::from_slice(&env, "Music"); // New category
-        let target: i128 = 2000;
-        let target1 = 300;
-        let deadline = 1692574408;
-        let image = String::from_slice(&env, "https://avatars.githubusercontent.com/u/43227117?s=400&u=f9fa09edf5ea7a342c28f54fd0da5a7ac53b3eed&v=4");
-
-        // create campaigns
-        let campaign1 = client.create_campaign(
-            &artist1,
-            &title,
-            &description,
-            &image,
-            &target,
-            &deadline,
-            &category,
-        );
-        let campaign2 = client.create_campaign(
-            &artist2,
-            &title,
-            &description,
-            &image,
-            &target1,
-            &deadline,
-            &category,
-        );
-
-        Self {
-            env: env,
-            client: client,
-            contract_token,
-            token,
-            arty_token,
-            artist1,
-            artist2,
-            donor1,
-            donor2,
-            title,
-            description,
-            target,
-            target1,
-            deadline,
-            image,
-            campaign1,
-            campaign2,
-            category,
-        }
-    }
+    let product = client.get_product(&1);
+    assert_eq!(product.remaining, target - 1);
 }
 
 #[test]
-fn test_all_created_campaigns() {
-    let setup = Setup::new();
+#[should_panic]
+fn test_initialize_twice() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    // Address::generate(&env);
 
-    let exp_campaign1 = Campaign {
-        id: 1,
-        owner: setup.artist1.clone(),
-        title: setup.title.clone(),
-        description: setup.description.clone(),
-        target: setup.target,
-        deadline: setup.deadline,
-        amount_collected: 0,
-        status: true,
-        image: setup.image.clone(),
-        category: setup.category.clone(), // New category field
-        donators: Vec::new(&setup.env.clone()),
-        donations: Vec::new(&setup.env.clone()),
-    };
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
 
-    let exp_campaign2 = Campaign {
-        id: 2,
-        owner: setup.artist2.clone(),
-        title: setup.title,
-        description: setup.description,
-        target: setup.target1,
-        deadline: setup.deadline,
-        amount_collected: 0,
-        status: true,
-        category: setup.category.clone(), // New category field
-        image: setup.image,
-        donators: Vec::new(&setup.env.clone()),
-        donations: Vec::new(&setup.env.clone()),
-    };
+    assert_eq!(initialized, String::from_str(&env, "Initialized"));
 
-    assert_eq!(setup.campaign1, exp_campaign1);
-    assert_eq!(setup.campaign2, exp_campaign2);
+    assert_eq!(
+        Ok(client.initialize(
+            &admin.clone(),
+            &admin.clone(),
+            &admin.clone(),
+            &admin.clone()
+        )),
+        Err(Error::AlreadyInitialized)
+    );
+}
+#[test]
+#[should_panic]
+fn test_create_product_with_past_expiry() {
+    let env = Env::default();
 
-    // Get all campaigns before donation
-    let all_campaings = setup.client.get_campaigns();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    // Address::generate(&env);
 
-    let mut all_camp_exp = Vec::new(&setup.env.clone()); // Initialize with a new address
-    all_camp_exp.push_back(exp_campaign1.clone());
-    all_camp_exp.push_back(exp_campaign2.clone());
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
 
-    assert_eq!(all_campaings, all_camp_exp);
+    assert_eq!(initialized, String::from_str(&env, "Initialized"));
+    env.ledger().set(LedgerInfo {
+        timestamp: 12345,
+        protocol_version: 1,
+        sequence_number: 10,
+        network_id: Default::default(),
+        base_reserve: 10,
+        max_entry_ttl: 1,
+        min_persistent_entry_ttl: 2,
+        min_temp_entry_ttl: 2,
+    });
+    std::println!("{}", env.ledger().timestamp());
+    let title = String::from_str(&env, "Product 1");
+    let description = String::from_str(&env, "Description 1");
+    let category = String::from_str(&env, "Category 1");
+    let expiry = env.ledger().timestamp() - 100;
+    let image = String::from_str(&env, "image.png");
+    let price = 1000;
+    let target = 10;
+
+    // initialize(env.clone(), admin.clone(), admin.clone(), admin.clone(), admin.clone());
+    let result = client.create_product(
+        // env.clone(),
+        &title.clone(),
+        &description.clone(),
+        &category.clone(),
+        &expiry,
+        &image.clone(),
+        &price,
+        &target,
+    );
+
+    assert_eq!(Ok(result), Err(Error::ExpiryShouldBeFuture));
 }
 
 #[test]
-fn test_donate_to_campaigns_and_payment_splitter() {
-    let setup = Setup::new();
+#[should_panic]
+fn test_get_discount_with_zero_amount() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Marketplace);
+    let client = MarketplaceClient::new(&env, &contract_id);
+    env.mock_all_auths();
+    let admin = Address::generate(&env);
+    let customer = Address::generate(&env);
 
-    let amount1: i128 = 100;
-    let splitted_amount1 =
-        setup
-            .client
-            .donate_to_campaign(&1, &setup.donor1, &amount1, &setup.contract_token.clone());
+    let title = String::from_str(&env, "Product 1");
+    let description = String::from_str(&env, "Description 1");
+    let category = String::from_str(&env, "Category 1");
+    let expiry = env.ledger().timestamp() + 10000;
+    let image = String::from_str(&env, "image.png");
+    let price = 1000;
+    let target = 10;
+    let amount = 0;
+    let token_id = Address::generate(&env);
 
-    assert_eq!(splitted_amount1, (95, 2, 3));
-    assert_eq!(setup.arty_token.balance(&setup.donor1), 100);
+    let initialized = client.initialize(
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+        &admin.clone(),
+    );
+    // std::println!("{:?}",initialized);
+    let token = token::StellarAssetClient::new(
+        &env,
+        &env.register_stellar_asset_contract(token_id.clone()),
+    );
 
-    let amount2: i128 = 150;
-    let splitted_amount2 =
-        setup
-            .client
-            .donate_to_campaign(&2, &setup.donor2, &amount2, &setup.contract_token.clone());
+    // Mint some ARTY tokens to work with
+    token.mint(&customer.clone(), &10000000000);
+    client.create_product(
+        // env.clone(),
+        &title.clone(),
+        &description.clone(),
+        &category.clone(),
+        &expiry,
+        &image.clone(),
+        &price,
+        &target,
+    );
 
-    assert_eq!(splitted_amount2, (142, 3, 4));
-    assert_eq!(setup.arty_token.balance(&setup.donor2), 150);
+    // let (reserve_amount, launchpad_amount, dev_amount) =
 
-    let amount3: i128 = 149;
-    let splitted_amount3 =
-        setup
-            .client
-            .donate_to_campaign(&2, &setup.donor1, &amount3, &setup.contract_token.clone());
-
-    assert_eq!(splitted_amount3, (141, 2, 4));
-    assert_eq!(setup.arty_token.balance(&setup.donor1), 249);
-
-    // Get Donators
-    let donators = setup.client.get_donators(&2);
-
-    let mut exp_donators = Vec::new(&setup.env);
-    exp_donators.push_front(setup.donor2.clone());
-    exp_donators.push_front(setup.donor1.clone());
-
-    assert_eq!(donators, exp_donators);
-
-    // Balance Test
-    let dev_acc = setup.client.get_dev_acc();
-    let launchpad_acc = setup.client.get_launchpad_acc();
-
-    assert_eq!(setup.token.balance(&dev_acc), 11);
-    assert_eq!(setup.token.balance(&launchpad_acc), 7);
-    assert_eq!(setup.token.balance(&setup.artist1), 95);
-    assert_eq!(setup.token.balance(&setup.artist2), 283);
-
-    assert_eq!(setup.arty_token.balance(&setup.donor2), 150);
+    assert_eq!(
+        Ok(client.get_discount(&1, &customer.clone(), &amount, &token.address.clone())),
+        Err(Error::AmountMustNonZero)
+    );
 }
